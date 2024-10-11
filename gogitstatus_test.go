1618b7d2bd1f195bfffafab178f9a1edd7ede5a6
kivattt <kivattt@gmail.com>
2024-10-11 17:22:06 +0200

    Remove commented code

--- gogitstatus_test.go
+++ gogitstatus_test.go
@@ -121,14 +121,3 @@ func TestParseGitIndex(t *testing.T) {
 		}
 	}
 }
-
-/*func TestStatus(t *testing.T) {
-	entries, err := Status(".")
-	if err != nil {
-		fmt.Println("error: " + err.Error())
-		return
-	}
-	for _, e := range entries {
-		fmt.Println("\x1b[0;31m" + e + "\x1b[0m")
-	}
-}*/
