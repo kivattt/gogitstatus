1745ddc49802d9ba028006fb8eb1bdf67ea04841
kivattt <kivattt@gmail.com>
2024-10-12 13:14:17 +0200

    Add TestStatusRaw

--- gogitstatus_test.go
+++ gogitstatus_test.go
@@ -12,8 +12,81 @@ import (
 	"testing"
 )
 
-/*func TestStatus(t *testing.T) {
-}*/
+func printRed(text string) {
+	fmt.Print("\x1b[31m" + text + "\x1b[0m")
+}
+
+func printGreen(text string) {
+	fmt.Print("\x1b[32m" + text + "\x1b[0m")
+}
+
+
+func TestStatusRaw(t *testing.T) {
+	testsPath := "./tests-statusraw"
+	tests, err := os.ReadDir(testsPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("\x1b[1;30mTestStatusRaw:\x1b[0m")
+
+	printPaths := func(entries []string) {
+		for _, e := range entries {
+			fmt.Println("    " + e)
+		}
+	}
+
+	for _, test := range tests {
+		filesPath := filepath.Join(testsPath, test.Name(), "files")
+		indexPath := filepath.Join(testsPath, test.Name(), "index")
+		expectedPath := filepath.Join(testsPath, test.Name(), "expected.txt")
+		fmt.Print("Test file " + expectedPath + ": ")
+
+		file, err := os.Open(expectedPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var expectedPaths []string
+		var expectedError error = nil
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.HasPrefix(line, "Error text:") {
+				expectedError = errors.New(line[len("Error text:"):])
+				break
+			}
+			expectedPaths = append(expectedPaths, line)
+		}
+
+		paths, err := StatusRaw(filesPath, indexPath)
+		if expectedError == nil && err != nil {
+			printRed("Failed\n")
+			t.Fatal("expected no error, but got: "+err.Error())
+		}
+
+		if err != nil && expectedError != nil {
+			if err.Error() != expectedError.Error() {
+				printRed("Failed\n")
+				t.Fatal("expected error text \""+expectedError.Error()+"\", but got: \""+err.Error()+"\"")
+			}
+		}
+
+		if !reflect.DeepEqual(paths, expectedPaths) {
+			printRed("Failed\n")
+
+			fmt.Println("Expected entries:")
+			printPaths(expectedPaths)
+			fmt.Println("But got:")
+			printPaths(paths)
+			t.Fatal("See above ^")
+		}
+
+		printGreen("Success\n")
+	}
+}
 
 func TestParseGitIndex(t *testing.T) {
 	testsPath := "./tests-index-parser"
@@ -21,6 +94,9 @@ func TestParseGitIndex(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	fmt.Println()
+	fmt.Println("\x1b[1;30mTestParseGitIndex:\x1b[0m")
 
 	printEntries := func(entries []GitIndexEntry) {
 		for _, e := range entries {
@@ -46,14 +122,6 @@ func TestParseGitIndex(t *testing.T) {
 		return true
 	}
 
-	printRed := func(text string) {
-		fmt.Print("\x1b[31m" + text + "\x1b[0m")
-	}
-
-	printGreen := func(text string) {
-		fmt.Print("\x1b[32m" + text + "\x1b[0m")
-	}
-
 	for _, version := range tests {
 		versionTests, err := os.ReadDir(filepath.Join(testsPath, version.Name()))
 		if err != nil {
@@ -63,13 +131,12 @@ func TestParseGitIndex(t *testing.T) {
 		for _, versionTest := range versionTests {
 			indexPath := filepath.Join(testsPath, version.Name(), versionTest.Name(), "index")
 			expectedPath := filepath.Join(testsPath, version.Name(), versionTest.Name(), "expected.txt")
-			inTest := "In test file " + expectedPath + ": "
 			fmt.Print("Test file " + expectedPath + ": ")
 
 			file, err := os.Open(expectedPath)
 			if err != nil {
 				printRed("Failed\n")
-				t.Fatal(inTest, err)
+				t.Fatal(err)
 			}
 
 			expectedEntries := []GitIndexEntry{}
@@ -90,7 +157,7 @@ func TestParseGitIndex(t *testing.T) {
 				sha1HashBytes, err := hex.DecodeString(sha1HashHex)
 				if err != nil {
 					printRed("Failed\n")
-					t.Fatal(inTest, err)
+					t.Fatal(err)
 				}
 
 				expectedEntries = append(expectedEntries, GitIndexEntry{Path: pathName, Hash: sha1HashBytes})
@@ -100,13 +167,13 @@ func TestParseGitIndex(t *testing.T) {
 			entries, err := ParseGitIndex(indexPath)
 			if expectedError == nil && err != nil {
 				printRed("Failed\n")
-				t.Fatal(inTest, "expected no error, but got: "+err.Error())
+				t.Fatal("expected no error, but got: "+err.Error())
 			}
 
 			if err != nil && expectedError != nil {
 				if err.Error() != expectedError.Error() {
 					printRed("Failed\n")
-					t.Fatal(inTest, "expected error text \""+expectedError.Error()+"\", but got: \""+err.Error()+"\"")
+					t.Fatal("expected error text \""+expectedError.Error()+"\", but got: \""+err.Error()+"\"")
 				}
 			}
 
@@ -117,7 +184,7 @@ func TestParseGitIndex(t *testing.T) {
 				printEntries(expectedEntries)
 				fmt.Println("But got:")
 				printEntries(entries)
-				t.Fatal(inTest, "See above ^")
+				t.Fatal("See above ^")
 			}
 
 			printGreen("Success\n")
