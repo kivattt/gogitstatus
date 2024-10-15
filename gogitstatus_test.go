11c567d8c88a34240e87d426d634bf1aac82f5b6
kivattt <kivattt@gmail.com>
2024-10-15 13:48:21 +0200

    Add test for ignoring files named .git, allow .zip files for test data

--- gogitstatus_test.go
+++ gogitstatus_test.go
@@ -1,10 +1,12 @@
 package gogitstatus
 
 import (
+	"archive/zip"
 	"bufio"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -35,6 +37,72 @@ func printGray(text string) {
 	} else {
 		fmt.Print("\x1b[1;30m" + text + "\x1b[0m")
 	}
+}
+
+// Copied from: https://stackoverflow.com/a/24792688
+func extractZipArchive(zipFilePath, destination string) error {
+	r, err := zip.OpenReader(zipFilePath)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(destination, 0755)
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(destination, f.Name)
+
+		// Check for ZipSlip (Directory traversal)
+		if !strings.HasPrefix(path, filepath.Clean(destination)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", path)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func TestStatusRaw(t *testing.T) {
@@ -106,6 +174,16 @@ func TestStatusRaw(t *testing.T) {
 			expectedChangedFiles = append(expectedChangedFiles, ChangedFile{Path: filepath.Join(filesPath, pathText), WhatChanged: StringToWhatChanged(whatChangedText)})
 		}
 
+		_, err = os.Stat(filesPath)
+		if err != nil {
+			zipFilePath := filepath.Join(testsPath, test.Name(), "files.zip")
+			err := extractZipArchive(zipFilePath, filesPath)
+			if err == nil {
+				defer func() {
+					os.RemoveAll(filesPath)
+				}()
+			}
+		}
 		changedFiles, err := StatusRaw(filesPath, indexPath)
 
 		if expectedAnyError && err == nil {
